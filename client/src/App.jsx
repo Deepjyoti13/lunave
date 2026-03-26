484b759f4dcefb1a7213c11df2292d65046528ef
deepjyoti-de <deepjyoti.de@logward.com>
2026-03-27 02:12:36 +0530

    feat: mount WishlistProvider and WishlistDrawer in App

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,9 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { AuthProvider } from './context/AuthContext'
 import { Toaster } from 'react-hot-toast'
 import { CartProvider } from './context/Cartcontext.jsx'
+import { WishlistProvider } from './context/WishlistContext.jsx'
 import CartDrawer from './components/Cartdrawer.jsx'
+import WishlistDrawer from './components/WishlistDrawer.jsx'
 import HomePage from './pages/HomePage.jsx'
 import AdminApp from './admin/AdminApp.jsx'
 import Register from './pages/Register.jsx'
@@ -13,19 +15,21 @@ export default function App() {
   return (
     <AuthProvider>
       <CartProvider>
-        <Toaster position="top-right" />   {/* 🔥 toast UI */}
-        <CartDrawer />
+        <WishlistProvider>
+          <Toaster position="top-right" />
+          <CartDrawer />
+          <WishlistDrawer />
 
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/admin/*" element={<AdminApp />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/profile" element={<Profile />} />
-          </Routes>
-        </BrowserRouter>
-
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/admin/*" element={<AdminApp />} />
+              <Route path="/register" element={<Register />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/profile" element={<Profile />} />
+            </Routes>
+          </BrowserRouter>
+        </WishlistProvider>
       </CartProvider>
     </AuthProvider>
   )
